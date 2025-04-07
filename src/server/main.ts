8d10f74e3d24b4e8c6167b860dbaf99bd72d15c0
Mark Schwartzkopf <mark@schwartzkopf.org>
2025-04-07 14:34:21 -0500

    remove osc

--- src/server/main.ts
+++ src/server/main.ts
@@ -2,7 +2,6 @@ import { initializeData } from './data.js';
 import { initializeGroups, initializePagesDir } from './file-manager.js';
 import { initializeServer } from './http-server.js';
 import { log } from './logger.js';
-import { initializeOSCServer } from './osc-server.js';
 
 initializeGroups()
   .then(() => {
@@ -13,9 +12,6 @@ initializeGroups()
   })
   .then(() => {
     return initializeServer();
-  })
-  .then(() => {
-    return initializeOSCServer();
   })
   .catch((err) => {
     log('error', 'Error initializing', err);
